e282f118c12233e0fea36de198ea47cf2bc4a5d4
HrvojeCosic <hrvojecosic333@gmail.com>
2024-02-05 02:37:13 +0100

    Merge shell scripts

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 2. Run the container using `sudo docker run --name metar_service_container -p 5432:5432 -d metar_service_image`
 
 ### Using the application
-* Run the job once using `./populate_metar_storage.sh` OR run it repeatedly using `./schedule_job.sh`
 * Run the tests using `./gradlew test` OR the service using `./gradlew bootRun`
+* Run the job scheduler using `./schedule_job.sh`
 
 ### Cleaning up
 * Clean up the container using `sudo docker rm -f metar_service_container`
--- schedule_job.sh
+++ schedule_job.sh
@@ -1,18 +1,44 @@
 #!/bin/bash
 
-# Check if jq is installed
+# Check if prerequisites are installed
 if ! command -v jq &> /dev/null; then
     echo "Error: jq is not installed. Please install jq before running the job."
     echo "On Debian/Ubuntu, you can install it using: sudo apt-get install jq"
     exit 1
 fi
 
-# Repeatedly call the job
+# Set environment variables
+METAR_STORAGE_BASE_URL="http://localhost:8080"
+METAR_SERVICE_URL="https://tgftp.nws.noaa.gov/data/observations/metar/stations/"
+METAR_ENDPOINT_TEMPLATE="${METAR_STORAGE_BASE_URL}/airport/{airport_code}/METAR"
+SUBSCRIPTION_ENDPOINT="${METAR_STORAGE_BASE_URL}/subscriptions"
+
+# Function to fetch and store METAR data for a given airport code
+fetch_and_store_metar() {
+    airport_code="$1"
+    metar_endpoint=$(echo "${METAR_ENDPOINT_TEMPLATE}" | sed "s/{airport_code}/${airport_code}/g")
+
+    fetched=$(curl -s "${METAR_SERVICE_URL}/${airport_code}.TXT")
+    METAR_data=$(echo "${fetched}" | sed -n '2p')
+    response=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"data\": \"${METAR_data}\"}" "${metar_endpoint}" 2>&1)
+    echo "METAR storage response: ${response}"
+}
+
+# Function to fetch all subscribed airports and call fetch_and_store_metar for each
+fetch_and_store_all_metar() {
+    subscribed_airports=$(curl -s -X GET -H "Content-Type: application/json" -d '{}' "${SUBSCRIPTION_ENDPOINT}")
+
+    jq -r '.[] | .icaoCode' <<< "${subscribed_airports}" | while read -r icaoCode; do
+        fetch_and_store_metar "${icaoCode}"
+    done
+}
+
+# Repeatedly call the fetch_and_store_all_metar function
 while true; do
     echo -n "Populating METAR storage: "
     date
 
-    ./populate_metar_storage.sh
+    fetch_and_store_all_metar
 
-    sleep 5
+    sleep 8
 done
